34bb0c0b8356f81a232a339ae71d4c58684db789
praisy <praisypaulin999@gmail.com>
2026-05-09 16:57:27 +0530

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-  output: 'server'
+  output: 'server',
+  adapter: cloudflare()
 });
